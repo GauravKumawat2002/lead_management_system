7b9eb074152a95ef7984d3b7841e4cacde3916ad
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-10-21 16:45:47 +0530

    Adds a index.d.ts file that holds all the type definition require in the project

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+import { SignInForm, SignUpForm } from "@/schemas/auth-form-schema";
+export global {
+  type FormType = "signIn" | "signUp";
+  type FormSchema = SignInForm | SignUpForm;
+
+  interface AuthFormProps {
+    type: FormType;
+  }
+}

56a111f9a25aebc410ecb745a8b22e8f07eb10da
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-02 17:07:26 +0530

    Feat: Type Definiton for AuthForm

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -1,6 +1,7 @@
 import { SignInForm, SignUpForm } from "@/schemas/auth-form-schema";
 export global {
   type FormType = "signIn" | "signUp";
+
   type FormSchema = SignInForm | SignUpForm;
 
   interface AuthFormProps {

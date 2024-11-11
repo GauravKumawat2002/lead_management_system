6789c6515f0bbc2cae574d8aad411538bc915e9f
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-11 13:36:25 +0530

    Feat: Add new lead form page

---
+++ app/(routes)/leads/new-lead/page.tsx
@@ -0,0 +1,10 @@
+import AddLeads from "@/components/custom/leads/new-lead/add-Leads-form";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <AddLeads />
+    </div>
+  );
+}

f7a6d18eb96c6af940619983ed0b0d8efd9610c1
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-11 13:14:53 +0530

    Makes the custom-form-field component more resuable by making it able to render different form fields based on their type

--- components/custom/shared/custom-form-field.tsx
+++ components/custom/shared/custom-form-field.tsx
@@ -1,27 +1,67 @@
-import React, { useState } from "react";
+"use client";
+import { useState } from "react";
 import { Control, FieldPath, FieldValues } from "react-hook-form";
-import { FormField, FormLabel, FormControl, FormMessage, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { format } from "date-fns";
+import { Eye, EyeOff, CalendarRangeIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 import { SignInForm, SignUpForm } from "@/schemas/auth-form-schema";
-import { Eye, EyeOff } from "lucide-react"; // Import icons from lucide-react
+import { AddLeadsForm } from "@/schemas/add-leads-form-schema";
+
+import {
+  FormField,
+  FormLabel,
+  FormControl,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 interface CustomFormFieldProps<T extends FieldValues> {
   control: Control<T>;
-  name: FieldPath<T>;
   label: string;
+  name: FieldPath<T>;
+  options?: { value: string; label: string }[];
   placeholder?: string;
+  type:
+    | "date"
+    | "number"
+    | "password"
+    | "select"
+    | "textarea"
+    | "text"
+    | string;
 }
 
-export default function CustomFormField<T extends SignInForm | SignUpForm>({
+export default function CustomFormField<
+  T extends SignInForm | SignUpForm | AddLeadsForm,
+>({
   control,
-  name,
   label,
+  name,
+  options,
   placeholder,
+  type,
 }: CustomFormFieldProps<T>) {
   const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
 
   const togglePasswordVisibility = () => {
-    setShowPassword(prevState => !prevState);
+    setShowPassword((prevState) => !prevState);
   };
 
   return (
@@ -30,34 +70,125 @@ export default function CustomFormField<T extends SignInForm | SignUpForm>({
       name={name}
       render={({ field }) => (
         <FormItem className="form-item pb-4">
-          <FormLabel className="form-label text-gray-700 mb-4" htmlFor={label}>
+          <FormLabel
+            className="form-label mb-4 text-gray-700 dark:text-gray-200"
+            htmlFor={label}
+          >
             {label} <span className="text-red-500">*</span>
-          </FormLabel>
-          <FormControl>
-            {/* Container for Input and Eye Icon */}
-            <div className="relative">
+          </FormLabel>{" "}
+          {type === "date" && (
+            <Popover>
+              <PopoverTrigger asChild className="block">
+                <FormControl>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-full pl-3 text-left font-normal",
+                      !field.value && "text-muted-foreground",
+                    )}
+                  >
+                    {field.value ? (
+                      format(new Date(field.value), "PPP")
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                    <CalendarRangeIcon className="-mt-5 ml-auto h-4 w-4 opacity-50" />
+                  </Button>
+                </FormControl>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="single"
+                  selected={field.value ? new Date(field.value) : undefined}
+                  onSelect={(date) => field.onChange(date)}
+                  // onSelect={(date) =>
+                  //   field.onChange(date ? date.toISOString() : "")
+                  // }
+                  disabled={(date) => date < new Date()}
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          )}
+          {type === "number" && (
+            <FormControl>
               <Input
-                className="input-class pr-10" // Add padding-right to make space for the icon
+                className="input-class pr-10"
                 {...field}
                 id={label}
                 placeholder={placeholder}
-                type={
-                  (name === "password" || name === "confirmPassword") && !showPassword
-                    ? "password"
-                    : "text"
-                }
+                type="number"
+                value={field.value as number | undefined}
+                onChange={(e) => field.onChange(Number(e.target.value))}
               />
-              {/* Toggle Button (eye icon inside the input field) */}
-              {(name === "password" || name === "confirmPassword") && (
+            </FormControl>
+          )}
+          {type === "password" && (
+            <FormControl>
+              <div className="relative">
+                <Input
+                  className="input-class pr-10"
+                  {...field}
+                  id={label}
+                  placeholder={placeholder}
+                  type={!showPassword ? "password" : "text"}
+                  value={field.value as string | undefined}
+                />
                 <button
                   type="button"
                   onClick={togglePasswordVisibility}
-                  className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500">
+                  className="absolute right-2 top-1/2 -translate-y-1/2 transform text-gray-500"
+                >
                   {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                 </button>
-              )}
-            </div>
-          </FormControl>
+              </div>
+            </FormControl>
+          )}
+          {type === "select" && (
+            <Select
+              onValueChange={field.onChange}
+              defaultValue={field.value as string}
+            >
+              <FormControl>
+                <SelectTrigger>
+                  <SelectValue
+                    placeholder={
+                      !placeholder ? "Please select a value" : placeholder
+                    }
+                  />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>
+                {options?.map((option) => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          )}
+          {type === "textarea" && (
+            <FormControl>
+              <Textarea
+                placeholder={!placeholder ? "Optional" : placeholder}
+                className="resize-none"
+                {...field}
+                value={field.value as string | undefined}
+              />
+            </FormControl>
+          )}
+          {type === "text" && (
+            <FormControl>
+              <Input
+                className="input-class pr-10"
+                {...field}
+                id={label}
+                placeholder={placeholder}
+                type="text"
+                value={field.value as string | undefined}
+              />
+            </FormControl>
+          )}
           <FormMessage {...field} className="form-message mt-2" />
         </FormItem>
       )}

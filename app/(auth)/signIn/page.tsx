9402de4bcc23e986553041e45af671143834037a
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-12 16:55:17 +0530

    Changed the folder name from sign-in to signIn

---
+++ app/(auth)/signIn/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import AuthForm from "@/components/custom/auth/auth-form";
+
+export default function SignIn() {
+  return (
+    <div>
+      <AuthForm type="signIn" />
+    </div>
+  );
+}

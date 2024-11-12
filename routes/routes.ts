0adcd9783673b74f2080b0fafb6492d47a731987
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-12 16:49:08 +0530

    Feat: routes object containing all the routes of the application

---
+++ routes/routes.ts
@@ -0,0 +1,14 @@
+export const ROUTES = {
+  HOME: "/",
+  LEADS: "/leads",
+  NEW_LEADS: "/leads/newLead",
+  LEADS_DETAILS: /\/leads\/leadDetail\/\w+/,
+  FOLLOW_UP_LEADS: "/leads/followUpLeads",
+  SIGNIN: "/signIn",
+  SIGNUP: "/signUP",
+  //   PROFILE: "/profile",
+  //   SETTINGS: "/settings",
+  //   FORGOT_PASSWORD: "/forgot-password",
+  //   RESET_PASSWORD: "/reset-password",
+  //   NOT_FOUND: "/404",
+};

6291e07b1e01f3ddd6d00c3fd8894d9a67fa7f50
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-12 16:52:53 +0530

    feat: add ActionButton component with dropdown menu for actions

---
+++ components/custom/shared/action-button.tsx
@@ -0,0 +1,36 @@
+"use client";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { LucideChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export type Action = {
+  title: string;
+  onClick: () => void;
+};
+export default function ActionButton({ actions }: { actions: Action[] }) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button className="btn btn-primary">
+          Actions
+          <LucideChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuGroup>
+          {actions.map((action) => (
+            <DropdownMenuItem onClick={action.onClick}>
+              {action.title}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}

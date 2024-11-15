9721023d72c17a27ea673c1b609a7bb7b80176de
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-15 12:40:59 +0530

    refactor: remove email field from signInSchema & enhance sign-up schema with password complexity requirements

--- schemas/auth-form-schema.ts
+++ schemas/auth-form-schema.ts
@@ -1,21 +1,37 @@
 import { z } from "zod";
 
 const signInSchema = z.object({
-  userName: z.string().min(3, { message: "Username must be atleast 3 characters." }),
-  email: z.string().email({ message: "Invalid email" }),
+  userName: z
+    .string()
+    .min(3, { message: "Username must be atleast 3 characters." }),
   password: z.string().min(8),
 });
 const signUpSchema = z
   .object({
-    userName: z.string().min(3, { message: "Username must be atleast 3 characters." }),
+    userName: z
+      .string()
+      .min(3, { message: "Username must be at least 3 characters." }),
     email: z.string().email({ message: "Invalid email" }),
-    password: z.string().min(8),
-    confirmPassword: z.string().min(8),
+    password: z
+      .string()
+      .min(8)
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter.",
+      })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter.",
+      })
+      .regex(/\d/, { message: "Password must contain at least one number." })
+      .regex(/[@$!%*?&#]/, {
+        message: "Password must contain at least one special character.",
+      }),
+    confirmPassword: z.string().min(8), // This field should not be sent during sign-up
   })
-  .refine(data => data.password === data.confirmPassword, {
+  .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
+
 type SignInForm = z.infer<typeof signInSchema>;
 type SignUpForm = z.infer<typeof signUpSchema>;
 

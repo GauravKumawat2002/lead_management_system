eff99ae90eeda24e5c73a0d806435999c9b680b7
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-20 16:33:27 +0530

    feat: add getErrorMessage utility function for improved error handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function getErrorMessage(
+  errorData: string,
+  errorMessages: { [key: string]: string },
+): string {
+  const errorMessageKey = Object.keys(errorMessages).find((key) =>
+    errorData.includes(key),
+  );
+  return errorMessageKey
+    ? errorMessages[errorMessageKey]
+    : "An unknown error occurred";
 }

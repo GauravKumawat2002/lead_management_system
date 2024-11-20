2458b84226f8751e20697441b93ea9b566a2a9a7
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-20 16:48:53 +0530

    feat: implement sign-up functionality with error handling and user notifications

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,51 @@
-import AuthForm from "@/components/custom/auth/auth-form";
-import React from "react";
+"use client";
 
-export default function page() {
+import AuthForm from "@/components/custom/auth/auth-form";
+import { useToast } from "@/hooks/use-toast";
+import { getErrorMessage } from "@/lib/utils";
+import { ROUTES } from "@/routes/routes";
+import { SignUpForm } from "@/schemas/auth-form-schema";
+import { signUpService } from "@/services/authService";
+import { useRouter } from "next/navigation";
+import React, { useCallback } from "react";
+
+const errorMessages = {
+  "Warning: Email already exists, try login !!!":
+    "Email already exists, try login !!!",
+};
+export default function Page() {
+  const router = useRouter();
+  const { toast } = useToast();
+  const onSubmit = useCallback(
+    async (data: FormSchema) => {
+      try {
+        const { data: response, error } = await signUpService(
+          data as SignUpForm,
+        );
+        if (error) throw error;
+        console.log(response);
+        toast({
+          title: "Success",
+          description:
+            "Please check your e-mail for account verification then log-in",
+        });
+        router.push(ROUTES.SIGNIN);
+      } catch (error: any) {
+        console.log(error);
+        toast({
+          title: "Error",
+          variant: "destructive",
+          description: getErrorMessage(error.data, errorMessages),
+          className: "text-lg font-semibold",
+        });
+      }
+    },
+    [router],
+  );
+
   return (
     <div>
-      <AuthForm type="signUp" />
+      <AuthForm type="signUp" onSubmit={onSubmit} />
     </div>
   );
 }

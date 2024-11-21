a3f0dcc5de72758e81284b754581d594d9e62fc9
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-21 22:07:00 +0530

    feat: implement lead submission handling with success and error toasts

--- app/(routes)/leads/new-lead/page.tsx
+++ app/(routes)/leads/new-lead/page.tsx
@@ -1,10 +1,46 @@
-import AddLeads from "@/components/custom/leads/new-lead/add-Leads-form";
-import React from "react";
+"use client";
+import { useToast } from "@/hooks/use-toast";
+import { useAddLeads } from "@/data/leads";
+import { AddLeadsForm } from "@/schemas/add-leads-form-schema";
+import AddLeads from "@/components/custom/leads/newLead/add-Leads-form";
 
 export default function page() {
+  const { toast } = useToast();
+  const { mutate, isSuccess } = useAddLeads();
+  function handleAddLead(data: AddLeadsForm, onReset: () => void) {
+    isSuccess;
+    mutate(data, {
+      onSuccess: (response) => {
+        if (response.status !== 200) {
+          toast({
+            title: response.data,
+            // description: "Error adding lead",
+            className: "text-xl font-semibold",
+            variant: "destructive",
+          });
+        }
+        if (response.status === 200) {
+          onReset();
+          toast({
+            title: response.data,
+            description: "Lead added successfully",
+            className: "text-xl font-semibold",
+          });
+        }
+      },
+      onError: (error) => {
+        toast({
+          title: error.message,
+          description: "Error adding lead",
+          className: "text-xl font-semibold",
+          variant: "destructive",
+        });
+      },
+    });
+  }
   return (
     <div>
-      <AddLeads />
+      <AddLeads onSubmit={handleAddLead} />
     </div>
   );
 }

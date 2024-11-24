616e90f37378e3df10116c5a8f1c011a67235fa3
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-24 12:22:59 +0530

    feat: add handleButtonInteraction utility for lead management actions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import { ROUTES } from "@/routes/routes";
+import { deleteLeadByIds } from "@/services/leadsService";
+import { Row } from "@tanstack/react-table";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -12,4 +15,38 @@ export function getErrorMessage(
     errorData.includes(key),
   );
   return errorMessageKey ? errorMessages[errorMessageKey] : errorData;
+}
+export async function handleButtonInteraction<TData extends LeadsTableData>(
+  action: "Delete" | "Add New",
+  type: "Lead" | "Itinary",
+  selectedRows: Row<TData>[],
+) {
+  // Determine actions based on type and action
+  if (type === "Lead") {
+    if (selectedRows.length) {
+      if (action === "Delete") {
+        const selectedLeadIds = selectedRows
+          .map((row) => row.original.leadId)
+          .filter(Boolean);
+        await deleteLeadByIds(selectedLeadIds);
+      }
+    }
+    if (action === "Add New") {
+      return ROUTES.NEW_LEADS;
+    }
+  }
+  // if (type === "Itinary") {
+  //   if (action === "Delete") {
+  //     const selectedItinaryIds = selectedRows
+  //       .map((row) => row.original.itenaryId)
+  //       .filter(Boolean);
+  //     await deleteItinaryByIds(selectedItinaryIds);
+  //   }
+  //   if (action === "Add New") {
+  //     return ROUTES.NEW_ITINARIES;
+  //   }
+  // }
+  else {
+    console.log("Unsuported type", type);
+  }
 }

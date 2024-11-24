67a616e45138f7f91cf7b4f7c63a656f0d2f9c76
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-24 12:21:25 +0530

    feat: add lead management services  for fetching, deleting, and retrieving leads by ID

--- services/leadsService.ts
+++ services/leadsService.ts
@@ -1,6 +1,7 @@
 "use server";
 import { AddLeadsForm } from "@/schemas/add-leads-form-schema";
 import httpClient from "./httpClient";
+
 type AddLeadsResponse =
   | {
       data: any;
@@ -11,10 +12,38 @@ type AddLeadsResponse =
       data: any;
       status: number;
     };
+type GetAllLeadsResponse =
+  | {
+      data: LeadData[];
+      status: number;
+      statusText: string;
+    }
+  | {
+      data: any;
+      status: number;
+    };
+type GetLeadByIdResponse =
+  | {
+      data: LeadData;
+      status: number;
+      statusText: string;
+    }
+  | {
+      data: any;
+      status: number;
+    };
+type deleteLeadByIdResponse =
+  | {
+      data: LeadData[];
+      status: number;
+      statusText: string;
+    }
+  | {
+      data: any;
+      status: number;
+    };
 
-export const addLead = async (
-  lead: AddLeadsForm,
-): Promise<AddLeadsResponse> => {
+async function addLead(lead: AddLeadsForm): Promise<AddLeadsResponse> {
   try {
     const response = await httpClient.post("/leads/create", lead);
     return {
@@ -25,4 +54,83 @@ export const addLead = async (
   } catch (error: any) {
     return { data: error.response.data, status: error.response.status };
   }
+}
+async function getAllLeads(): Promise<GetAllLeadsResponse> {
+  try {
+    const response = await httpClient.get("/leads/fetch");
+    console.dir(response.data, { depth: null });
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  } catch (error: any) {
+    console.error(error);
+    return { data: error.response.data, status: error.response.status };
+  }
+}
+async function getLeadById(leadId: string): Promise<GetLeadByIdResponse> {
+  try {
+    const response = await httpClient.get(`/leads/fetch-by-id`, {
+      params: { leadId },
+    });
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  } catch (error: any) {
+    return { data: error.response.data, status: error.response.status };
+  }
+}
+async function deleteAllLeads() {
+  try {
+    const response = await httpClient.delete("/leads/delete-all");
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  } catch (error: any) {
+    return { data: error.response.data, status: error.response.status };
+  }
+}
+async function deleteLeadById(leadId: string): Promise<deleteLeadByIdResponse> {
+  try {
+    const response = await httpClient.delete(`/leads/delete-by-id`, {
+      params: { leadId },
+    });
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  } catch (error: any) {
+    return { data: error.response.data, status: error.response.status };
+  }
+}
+async function deleteLeadByIds(
+  leadIds: string[],
+): Promise<deleteLeadByIdResponse> {
+  try {
+    const response = await httpClient.delete(`/leads/delete-by-ids`, {
+      params: { leadIds: leadIds.join(",") },
+    });
+    return {
+      data: response.data,
+      status: response.status,
+      statusText: response.statusText,
+    };
+  } catch (error: any) {
+    return { data: error.response.data, status: error.response.status };
+  }
+}
+
+export {
+  addLead,
+  getAllLeads,
+  getLeadById,
+  deleteAllLeads,
+  deleteLeadById,
+  deleteLeadByIds,
 };

d53dda8a3bcd2fa4e25a970792dd367067d8b8f4
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-24 12:13:30 +0530

    feat: add utility types for converting object keys from snake_case to camelCase in Leads data types

--- types/leads.d.ts
+++ types/leads.d.ts
@@ -1,15 +1,30 @@
 import { AddLeadsForm } from "@/schemas/add-leads-form-schema";
+type ConvertSnakeToCamel<StringType extends string> =
+  StringType extends `${infer FirstPart}_${infer RestParts}`
+    ? `${FirstPart}${Capitalize<ConvertSnakeToCamel<RestParts>>}`
+    : StringType;
+
+type ConvertObjectKeysToCamel<ObjectType> = {
+  [KeyName in keyof ObjectType as ConvertSnakeToCamel<
+    string & KeyName
+  >]: ObjectType[KeyName];
+};
+
 export global {
-  type LeadsData = Omit<
-    AddLeadsForm,
-    | "budget_per_adult"
-    | "budget_per_child"
-    | "follow_up"
-    | "no_of_adults"
-    | "no_of_children"
-    | "planned_travel_date"
-    | "destination"
-  > & {
-    id: string;
+  type LeadsTableData = ConvertObjectKeysToCamel<
+    Omit<
+      AddLeadsForm,
+      | "budget_per_adult"
+      | "budget_per_child"
+      | "follow_up"
+      | "no_of_adults"
+      | "no_of_children"
+      | "planned_travel_date"
+    >
+  > & { leadId: string };
+  type LeadData = ConvertObjectKeysToCamel<AddLeadsForm> & {
+    leadId: string;
+    createdAt: string;
+    updatedAt: string;
   };
 }

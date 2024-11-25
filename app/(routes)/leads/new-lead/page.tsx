aa458722b747a60e55a5abe79eb7fcda7c9315d6
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-11-25 15:09:02 +0530

    refactor: remove unused isSuccess variable in handleAddLead function

--- app/(routes)/leads/new-lead/page.tsx
+++ app/(routes)/leads/new-lead/page.tsx
@@ -6,15 +6,13 @@ import AddLeads from "@/components/custom/leads/newLead/add-Leads-form";
 
 export default function page() {
   const { toast } = useToast();
-  const { mutate, isSuccess } = useAddLeads();
+  const { mutate } = useAddLeads();
   function handleAddLead(data: AddLeadsForm, onReset: () => void) {
-    isSuccess;
     mutate(data, {
       onSuccess: (response) => {
         if (response.status !== 200) {
           toast({
             title: response.data,
-            // description: "Error adding lead",
             className: "text-xl font-semibold",
             variant: "destructive",
           });

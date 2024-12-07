5c60b5a9c7d72f1133e88d538ed14b28e8e1ee52
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-07 20:37:47 +0530

    refactor: enhance middleware for improved JWT and refresh token handling during logout and token refresh

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,60 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { ROUTES } from "./routes/routes";
+import { logOutService, refreshTokenService } from "./services/authService";
+import { extractTokenExpirationTime } from "./lib/tokenStorage";
 
-export function middleware(request: NextRequest) {
-  // logic for invalidting the jwtToken cookie and redirecting to sign-in page
+export async function middleware(request: NextRequest) {
+  // logic for invalidting the jwtToken cookie and redirecting to sign-in page when the log-out route is hit
   if (request.nextUrl.pathname === ROUTES.LOGOUT) {
+    try {
+      const res = await logOutService();
+      if (res.status !== 200) {
+        throw res;
+      }
+    } catch (error: any) {
+      console.error(error.data);
+    }
+
     const response = NextResponse.redirect(new URL(ROUTES.SIGNIN, request.url));
-    response.cookies.set("jwtToken", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: -1, // Expire the cookie immediately
-      path: "/", // Ensure it applies globally
-    });
+    response.cookies.delete("refreshToken");
+    response.cookies.delete("jwtToken");
     return response;
   }
 
-  // logic for checking the jwtToken cookie and redirecting to sign-in page
-  const token = request.cookies.get("jwtToken");
+  const refreshToken = request.cookies.get("refreshToken");
+  const jwtToken = request.cookies.get("jwtToken");
 
-  if (
-    !token &&
-    (request.nextUrl.pathname === ROUTES.SIGNIN ||
-      request.nextUrl.pathname === ROUTES.SIGNUP)
-  ) {
-    return NextResponse.next();
+  // logic for checking the jwtToken cookie & refreshing the token
+  if (!jwtToken) {
+    try {
+      const result = await refreshTokenService();
+      if (!("jwtToken" in result)) {
+        throw result;
+      }
+      const response = NextResponse.next();
+      response.cookies.set("jwtToken", result.jwtToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        path: "/",
+        maxAge: extractTokenExpirationTime(result.jwtToken),
+      });
+      return response;
+    } catch (error) {
+      console.error(error);
+      return NextResponse.redirect(new URL(ROUTES.SIGNIN, request.url));
+    }
   }
-  if (!token) {
+
+  // logic for checking the refreshToken cookie and redirecting to sign-in or sign-up page
+  if (!refreshToken) {
+    if (
+      request.nextUrl.pathname === ROUTES.SIGNIN ||
+      request.nextUrl.pathname === ROUTES.SIGNUP
+    ) {
+      return NextResponse.next();
+    }
     return NextResponse.redirect(new URL(ROUTES.SIGNIN, request.url));
   }
 

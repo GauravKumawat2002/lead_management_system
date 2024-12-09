52f8ec1c6bcec439e256c6405ac8d8b269223b8b
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-09 17:35:50 +0530

    feat: enhance lead retrieval with error handling and display

--- app/(routes)/leads/page.tsx
+++ app/(routes)/leads/page.tsx
@@ -1,14 +1,20 @@
 import { columns } from "@/components/custom/leads/columns";
 import { DataTable } from "@/components/custom/leads/data-table";
+import ErrorDisplay from "@/components/custom/shared/ErrorDisplay";
 import { getAllLeads } from "@/services/leadsService";
 
 export default async function page() {
-  const { data: leads } = await getAllLeads();
-  return (
-    <>
-      <div className="mx-auto">
-        <DataTable columns={columns} data={leads} />
-      </div>
-    </>
-  );
+  try {
+    const response = await getAllLeads();
+    if (response.status !== 200) throw response.data;
+    return (
+      <>
+        <div className="mx-auto">
+          <DataTable columns={columns} data={response.data} />
+        </div>
+      </>
+    );
+  } catch (error) {
+    return <ErrorDisplay message={error as string} />;
+  }
 }

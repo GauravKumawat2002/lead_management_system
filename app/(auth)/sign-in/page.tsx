2a1c201c28823f8ac90968041e277f7f2c73f418
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-09 17:36:20 +0530

    feat: update sign-in process to use cookies for token storage and improve error handling

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,19 +1,13 @@
 "use client";
 import AuthForm from "@/components/custom/auth/auth-form";
 import { useToast } from "@/hooks/use-toast";
-import { storeToken } from "@/lib/tokenStorage";
+import { setTokenCookie } from "@/lib/tokenStorage";
+import { extractTokenExpirationTime } from "@/lib/tokenStorage";
 import { ROUTES } from "@/routes/routes";
 import { SignInForm } from "@/schemas/auth-form-schema";
 import { signInService } from "@/services/authService";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
-
-// "Error: User not found by Email: ": "User not found by Email. Please Sign Up",
-// refreshTOken Api: Error: Invalid Refresh Token
-// this response is for all api that has jwt token in header or autherror
-// Access denied !! Full authentication is required to access this resource
-// SignUP: Email already exists, try login !!!
-// get lead BY ID: Error: Lead not found
 
 export default function SignIn() {
   const router = useRouter();
@@ -26,13 +20,26 @@ export default function SignIn() {
           throw response.error;
         }
         if ("data" in response) {
-          storeToken(response.data.jwtToken);
+          const { jwtToken, refreshToken } = response.data;
+          setTokenCookie(
+            "jwtToken",
+            jwtToken,
+            extractTokenExpirationTime(jwtToken),
+          );
+          setTokenCookie(
+            "refreshToken",
+            refreshToken,
+            extractTokenExpirationTime(refreshToken),
+          );
           router.push(ROUTES.HOME);
         }
       } catch (error: any) {
         toast({
           title: "Error",
-          description: error.data,
+          description:
+            error.data === "Error: unknown error"
+              ? "Please verify your email and try again"
+              : error.data,
           variant: "destructive",
           className: "text-xl font-semibold",
         });
@@ -47,6 +54,3 @@ export default function SignIn() {
     </div>
   );
 }
-// Possible error messages
-// 1. Error: Invalid JWTHeader
-// 2. User not Signed-in!!! -> Unable to sign-in! Please try again.Unable to sign-in! Please try again.

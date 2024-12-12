c43d02ec8fae20f6cb45b798e007dad801999db0
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-12 17:44:27 +0530

    refactor: add new routes for itineraries and quotations in routes configuration

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,10 +1,26 @@
 export const ROUTES = {
   HOME: "/",
+  // routes for leads
   LEADS: "/leads",
   NEW_LEADS: "/leads/new-lead",
   LEADS_DETAILS: (leadId: string) => `/leads/lead-detail/${leadId}`,
   UPDATE_LEAD: (leadId: string) => `/leads/update-lead/${leadId}`,
   FOLLOW_UP_LEADS: "/leads/follow-up-leads",
+  // routes for itineraries
+  ITINERARY: "/proposals/itinerary",
+  NEW_ITINERARY: "/proposals/itinerary/new-itinerary",
+  UPDATE_ITINERARY: (itineraryId: string) =>
+    `/proposals/itinerary/update-itinerary/${itineraryId}`,
+  DUPLICATE_ITINERARY: (itineraryId: string) =>
+    `/proposals/itinerary/duplicate-itinerary/${itineraryId}`,
+  // routes for quotaions
+  QUOTATIONS: "/proposals/quotations",
+  NEW_QUOTATION: "/proposals/quotations/new-quotation",
+  UPDATE_QUOTATION: (quotationId: string) =>
+    `/proposals/quotations/update-quotation/${quotationId}`,
+  DUPLICATE_QUOTATION: (quotationId: string) =>
+    `/proposals/quotations/duplicate-quotation/${quotationId}`,
+  // routes for auth
   SIGNIN: "/sign-in",
   SIGNUP: "/sign-up",
   LOGOUT: "/log-out",

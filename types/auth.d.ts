26493b92d0ad992cf1c215d4fc0cd0db5b9273c9
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-21 11:00:42 +0530

    feat: Extend auth form types to include reset password schemas

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -1,11 +1,27 @@
-import { SignInForm, SignUpForm } from "@/schemas/auth-form-schema";
+import {
+  SignInForm,
+  SignUpForm,
+  ConfirmResetPasswordSchemaType,
+  RequestResetPasswordSchemaType,
+} from "@/schemas/auth-form-schema";
 export global {
-  type FormType = "signIn" | "signUp";
+  type FormType =
+    | "signIn"
+    | "signUp"
+    | "requestResetPassword"
+    | "confirmResetPassword";
 
-  type FormSchema = SignInForm | SignUpForm;
-
+  type FormSchema =
+    | SignInForm
+    | SignUpForm
+    | RequestResetPasswordSchemaType
+    | ConfirmResetPasswordSchemaType;
   interface AuthFormProps {
     type: FormType;
     onSubmit: (data: FormSchema) => void;
+  }
+  interface ResetPasswordFormProps {
+    type: FormType;
+    onSubmit?: (data: FormSchema) => void;
   }
 }

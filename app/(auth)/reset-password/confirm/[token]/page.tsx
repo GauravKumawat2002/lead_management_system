7746f8475ad29624a8c353bfac09f8792d519152
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-22 17:35:49 +0530

    feat: Refactor ConfirmResetPasswordPage to use useResetPassword hook for form submission

--- app/(auth)/reset-password/confirm/[token]/page.tsx
+++ app/(auth)/reset-password/confirm/[token]/page.tsx
@@ -1,45 +1,12 @@
 "use client";
 import ResetPasswordForm from "@/components/custom/auth/reset-password-form";
-import { useToast } from "@/hooks/use-toast";
-import { ROUTES } from "@/routes/routes";
-import { ConfirmResetPasswordSchemaType } from "@/schemas/auth-form-schema";
-import { confirmResetPasswordService } from "@/services/authService";
-import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useResetPassword } from "@/data/auth";
 
 export default function ConfirmResetPasswordPage({
   params,
 }: {
   params: { token: string };
 }) {
-  const { toast } = useToast();
-  const router = useRouter();
-  const onSubmit = useCallback(
-    async (data: FormSchema, onReset?: () => void) => {
-      try {
-        const { data: response } = await confirmResetPasswordService(
-          data as ConfirmResetPasswordSchemaType,
-          params.token,
-        );
-        // if (status !== 200) throw response;
-        console.log(response);
-        onReset && onReset();
-        toast({
-          title: "Success",
-          description: response,
-        });
-        router.push(ROUTES.SIGNIN);
-      } catch (error: any) {
-        console.log(error);
-        toast({
-          title: "Error",
-          variant: "destructive",
-          description: error.data,
-          className: "text-lg font-semibold",
-        });
-      }
-    },
-    [],
-  );
+  const { onSubmit } = useResetPassword("confirm", params.token);
   return <ResetPasswordForm type="confirmResetPassword" onSubmit={onSubmit} />;
 }

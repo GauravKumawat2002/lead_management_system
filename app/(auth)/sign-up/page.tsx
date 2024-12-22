53309b9958ec6173f76dffdd358537f16889d4eb
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-22 17:58:23 +0530

    feat: Refactor sign-up page to use useAuth hook for form submission

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,42 +1,10 @@
 "use client";
 
 import AuthForm from "@/components/custom/auth/auth-form";
-import { useToast } from "@/hooks/use-toast";
-import { ROUTES } from "@/routes/routes";
-import { SignUpForm } from "@/schemas/auth-form-schema";
-import { signUpService } from "@/services/authService";
-import { useRouter } from "next/navigation";
-import React, { useCallback } from "react";
+import { useAuth } from "@/data/auth";
 
 export default function Page() {
-  const router = useRouter();
-  const { toast } = useToast();
-  const onSubmit = useCallback(
-    async (data: FormSchema) => {
-      try {
-        const { data: response, error } = await signUpService(
-          data as SignUpForm,
-        );
-        if (error) throw error;
-        console.log(response);
-        toast({
-          title: "Success",
-          description:
-            "Please check your e-mail for account verification then log-in",
-        });
-        router.push(ROUTES.SIGNIN);
-      } catch (error: any) {
-        console.log(error);
-        toast({
-          title: "Error",
-          variant: "destructive",
-          description: error.data,
-          className: "text-lg font-semibold",
-        });
-      }
-    },
-    [router],
-  );
+  const { onSubmit } = useAuth("sign-up");
 
   return (
     <div>

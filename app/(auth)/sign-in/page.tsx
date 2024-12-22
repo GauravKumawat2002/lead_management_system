4d37c7cb9e736262d099481188875c2030a709f3
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-22 17:58:43 +0530

    feat: Refactor SignIn component to utilize useAuth hook for form submission

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,53 +1,9 @@
 "use client";
 import AuthForm from "@/components/custom/auth/auth-form";
-import { useToast } from "@/hooks/use-toast";
-import { setTokenCookie } from "@/lib/tokenStorage";
-import { extractTokenExpirationTime } from "@/lib/tokenStorage";
-import { ROUTES } from "@/routes/routes";
-import { SignInForm } from "@/schemas/auth-form-schema";
-import { signInService } from "@/services/authService";
-import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useAuth } from "@/data/auth";
 
 export default function SignIn() {
-  const router = useRouter();
-  const { toast } = useToast();
-  const onSubmit = useCallback(
-    async (data: FormSchema) => {
-      try {
-        const response = await signInService(data as SignInForm);
-        if ("error" in response) {
-          throw response.error;
-        }
-        if ("data" in response) {
-          const { jwtToken, refreshToken } = response.data;
-          setTokenCookie(
-            "jwtToken",
-            jwtToken,
-            extractTokenExpirationTime(jwtToken),
-          );
-          setTokenCookie(
-            "refreshToken",
-            refreshToken,
-            extractTokenExpirationTime(refreshToken),
-          );
-          router.push(ROUTES.HOME);
-        }
-      } catch (error: any) {
-        toast({
-          title: "Error",
-          description:
-            error.data === "Error: unknown error"
-              ? "Please verify your email and try again"
-              : error.data,
-          variant: "destructive",
-          className: "text-xl font-semibold",
-        });
-      }
-    },
-    [router, toast],
-  );
-
+  const { onSubmit } = useAuth("sign-in");
   return (
     <div>
       <AuthForm type="signIn" onSubmit={onSubmit} />

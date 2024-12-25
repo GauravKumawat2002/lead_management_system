8f2d9fc7705157e9183d25384cabec0f7c55fa7f
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-25 17:13:36 +0530

    feat: Replace loading fallback with LoadingSpinner component in RequestResetPasswordPage

--- app/(auth)/reset-password/request/page.tsx
+++ app/(auth)/reset-password/request/page.tsx
@@ -2,6 +2,7 @@
 import React, { lazy, Suspense } from "react";
 
 import { useResetPassword } from "@/data/auth";
+import LoadingSpinner from "@/components/custom/shared/loading-spinner";
 
 const ResetPasswordForm = lazy(
   () => import("@/components/custom/auth/reset-password-form"),
@@ -11,7 +12,7 @@ export default function RequestResetPasswordPage() {
   const { onSubmit } = useResetPassword("request");
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingSpinner />}>
       <ResetPasswordForm type="requestResetPassword" onSubmit={onSubmit} />
     </Suspense>
   );

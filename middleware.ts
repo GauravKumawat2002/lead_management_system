ba5d1bac2c1cfe126dbc8805bf0853933477bfaf
Gaurav Kumawat <gauravkumawat830@gmail.com>
2024-12-26 20:29:38 +0530

    feat: Extend middleware path matcher to include proposals

--- middleware.ts
+++ middleware.ts
@@ -63,5 +63,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/leads/:path*", "/log-out"],
+  matcher: ["/", "/leads/:path*", "/log-out", "/proposals/:path*"],
 };

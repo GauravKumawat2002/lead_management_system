8e383fc58fd99a7e363dee9615014e003f5b85d8
Gaurav Kumawat <gauravkumawat830@gmail.com>
2025-01-17 14:45:53 +0530

    feat: Rename page component to UpdateLeadPage and streamline error handling

--- app/(routes)/leads/page.tsx
+++ app/(routes)/leads/page.tsx
@@ -3,22 +3,18 @@ import { DataTable } from "@/components/custom/leads/data-table";
 import ErrorDisplay from "@/components/custom/shared/error-display";
 import { getAllLeads } from "@/services/leadsService";
 
-export default async function page() {
-  try {
-    const response = await getAllLeads();
-    if (response.status !== 200) throw response.data;
-    return (
-      <>
-        <div className="mx-auto">
-          <DataTable
-            columns={columns}
-            data={response.data}
-            handleButtonInteractionType="Lead"
-          />
-        </div>
-      </>
-    );
-  } catch (error) {
-    return <ErrorDisplay message={error as string} />;
-  }
+export default async function UpdateLeadPage() {
+  const response = await getAllLeads();
+  if (response.status !== 200) return <ErrorDisplay message={response.data} />;
+  return (
+    <>
+      <div className="mx-auto">
+        <DataTable
+          columns={columns}
+          data={response.data}
+          handleButtonInteractionType="Lead"
+        />
+      </div>
+    </>
+  );
 }

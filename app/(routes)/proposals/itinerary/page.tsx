2ad91c63eb638a825bdabd8da87f0ae3289d7e0e
Gaurav Kumawat <gauravkumawat830@gmail.com>
2025-01-17 14:49:22 +0530

    fix: Refactor ItineraryPage to improve error handling and update column import

--- app/(routes)/proposals/itinerary/page.tsx
+++ app/(routes)/proposals/itinerary/page.tsx
@@ -1,24 +1,20 @@
-import { columns } from "@/components/custom/proposals/itinerary/columns";
+import { Columns } from "@/components/custom/proposals/itinerary/columns";
 import { DataTable } from "@/components/custom/leads/data-table";
 import ErrorDisplay from "@/components/custom/shared/error-display";
 import { getAllItinerary } from "@/services/itineraryService";
 
 export default async function ItineraryPage() {
-  try {
-    const response = await getAllItinerary();
-    if (response.status !== 200) throw response.data;
-    return (
-      <>
-        <div className="mx-auto">
-          <DataTable
-            columns={columns}
-            data={response.data}
-            handleButtonInteractionType="Itinerary"
-          />
-        </div>
-      </>
-    );
-  } catch (error) {
-    return <ErrorDisplay message={error as string} />;
-  }
+  const response = await getAllItinerary();
+  if (response.status !== 200) return <ErrorDisplay message={response.data} />;
+  return (
+    <>
+      <div className="mx-auto">
+        <DataTable
+          columns={Columns}
+          data={response.data}
+          handleButtonInteractionType="Itinerary"
+        />
+      </div>
+    </>
+  );
 }
